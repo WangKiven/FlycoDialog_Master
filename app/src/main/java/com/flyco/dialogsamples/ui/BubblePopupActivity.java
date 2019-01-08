6110002194211b50c62cac4aabd6f7c474cbe89c
kivenspc\wangkiven <984768569@qq.com>
2019-01-08 16:21:02 +0800

    *

--- app/src/main/java/com/flyco/dialogsamples/ui/BubblePopupActivity.java
+++ app/src/main/java/com/flyco/dialogsamples/ui/BubblePopupActivity.java
@@ -20,20 +20,20 @@ import com.flyco.dialogsamples.R;
 import com.flyco.dialogsamples.extra.CustomBubblePopup;
 import com.flyco.dialogsamples.utils.T;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class BubblePopupActivity extends AppCompatActivity {
-    @Bind(R.id.tv_top_left)
+    @BindView(R.id.tv_top_left)
     TextView mTvTopLeft;
-    @Bind(R.id.tv_top_right)
+    @BindView(R.id.tv_top_right)
     TextView mTvTopRight;
-    @Bind(R.id.tv_bottom_left)
+    @BindView(R.id.tv_bottom_left)
     TextView mTvBottomLeft;
-    @Bind(R.id.tv_bottom_right)
+    @BindView(R.id.tv_bottom_right)
     TextView mTvBottomRight;
-    @Bind(R.id.tv_center)
+    @BindView(R.id.tv_center)
     TextView mTvCenter;
     private Context mContext = this;
 
@@ -58,7 +58,7 @@ public class BubblePopupActivity extends AppCompatActivity {
     @OnClick(R.id.tv_top_left)
     void clickTopLeftBtn() {
         View inflate = View.inflate(mContext, R.layout.popup_bubble_text, null);
-        TextView tv = ButterKnife.findById(inflate, R.id.tv_bubble);
+        TextView tv = inflate.findViewById(R.id.tv_bubble);
         BubblePopup bubblePopup = new BubblePopup(mContext, inflate);
         tv.setText("最美的不是下雨天,是曾与你躲过雨的屋檐~");
         bubblePopup.anchorView(mTvTopLeft)
